9fda234cba667987f9f5220c1d2b4d7f47763f1c
MSena <campsensi@gmail.com>
2015-01-24 23:25:26 +0000

    makeCacheMatrix
    caches the inverse of a matrix using solve()

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inverse <- NULL   ## creates a object in makeCaheMatrix's environment
+print(environment())
+evn <- environment()
+print(parent.env(evn))
+set <- function(y) {    
+      x <<- y                    ##this is used, eventually, change the matrix x for y, with ...$set(y)
+      inverse <<- NULL           ##this resets the inverse.
+}
+get <- function() x              ##writes matrix x to memory
+setsolve <- function(solve) inverse <<- solve
+getsolve <- function() inverse
+getevn <- function() environment()
+list(set = set,get = get,
+     setsolve = setsolve,
+     getsolve = getsolve,
+     getevn = getevn) ##this command creates a named list of 4 functions, holding the functions with the same name
 }
 
 
@@ -12,4 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      inverse <- x$getsolve()
+      if(!is.null(inverse)) {
+            message("getting cached data")
+            return(inverse)
+      }
+      data <- x$get()
+      inverse <- source(data, ...)
+      x$setsolve(inverse)
+      inverse
 }
